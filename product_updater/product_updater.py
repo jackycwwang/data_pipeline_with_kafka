a1f44f7399946911e0f411a735fe14f7dc79d5e9
Jacky Wang <jacky@lemay.ai>
2023-10-07 17:46:43 -0400

    minor updates

--- product_updater/product_updater.py
+++ product_updater/product_updater.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://mysql:mysql@localhost:3307/buy_online_db")
+# remote_url="172.25.184.208"
+remote_url="localhost"
+engine = create_engine(f"mysql+pymysql://mysql:mysql@{remote_url}:3307/buy_online_db")
 
 connection = engine.connect()
 
@@ -10,8 +12,24 @@ query = text("SHOW DATABASES")
 result = connection.execute(query)
 
 # Iterate through the result and print the database names
-for row in result:
-    print(row[0])
+# for row in result:
+    # print(row[0])
+
+file_path = './data/1429_1.csv'
+
+chunk_size = 50
+
+# Define a generator function to yield chunks of data
+def chunked_data_generator():
+    for chunk_df in pd.read_csv(file_path, chunksize=chunk_size, encoding='utf-8'):
+        yield chunk_df
+
+# Create a generator object
+data_generator = chunked_data_generator()
+
+# Iterate over the generator to get chunks of 50 rows at a time
+for chunk in data_generator:
+    # Process each chunk here
+    print(type(chunk))  # Example: Print the first 5 rows of each chunk
 
 
-# df = pd.read_csv("")
