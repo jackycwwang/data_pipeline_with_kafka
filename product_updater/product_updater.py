e0e0bf522252a331df4c16553cca256bc505ec2f
Jacky Wang <jacky@lemay.ai>
2023-10-07 17:54:48 -0400

    minor updates

--- product_updater/product_updater.py
+++ product_updater/product_updater.py
@@ -16,20 +16,9 @@ result = connection.execute(query)
     # print(row[0])
 
 file_path = './data/1429_1.csv'
+chunk_size = 10
 
-chunk_size = 50
-
-# Define a generator function to yield chunks of data
-def chunked_data_generator():
-    for chunk_df in pd.read_csv(file_path, chunksize=chunk_size, encoding='utf-8'):
-        yield chunk_df
-
-# Create a generator object
-data_generator = chunked_data_generator()
-
-# Iterate over the generator to get chunks of 50 rows at a time
-for chunk in data_generator:
-    # Process each chunk here
-    print(type(chunk))  # Example: Print the first 5 rows of each chunk
-
-
+with pd.read_csv(file_path, chunksize=chunk_size) as reader:
+    # print(reader)
+    for chunk in reader:
+        print(chunk)
