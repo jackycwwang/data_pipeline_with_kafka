1d72a6fca3043d0771a3bb401e2e1b760df1f99c
Jacky Wang <jacky@lemay.ai>
2023-10-08 14:36:09 -0400

    updater completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Data Pipeline - Kafka
+
+Dataset: [Walmart Product Data 2019](https://www.kaggle.com/datasets/promptcloud/walmart-product-data-2019)
--- product_updater/product_updater.py
+++ product_updater/product_updater.py
@@ -1,24 +1,83 @@
 import pandas as pd
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, Table, Column, Integer, String, Float, TIMESTAMP, MetaData
+from sqlalchemy.sql import text
+import pymysql
+from datetime import datetime
+import logging
+from tqdm import tqdm
 
-# remote_url="172.25.184.208"
-remote_url="localhost"
-engine = create_engine(f"mysql+pymysql://mysql:mysql@{remote_url}:3307/buy_online_db")
+logger = logging.getLogger("my_logger")
 
-connection = engine.connect()
+try:
+  engine = create_engine("mysql+pymysql://mysql:mysql@localhost:3307/buy_online_db")
+  connection = engine.connect()
+except pymysql.Error:
+  logger.error(f"Database connection error: {err}")
 
-# Execute a query to fetch the list of databases
-query = text("SHOW DATABASES")
+# Drop the table if it already existed
+tbl_name = 'product'
+query = text(f"drop table if exists {tbl_name}")
 result = connection.execute(query)
+connection.commit()
 
-# Iterate through the result and print the database names
-# for row in result:
-    # print(row[0])
+# Define the metadata
+metadata = MetaData()
 
-file_path = './data/1429_1.csv'
-chunk_size = 10
+# Define the product table schema
+product_table = Table('product', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(300)),
+    Column('category', String(500)),
+    Column('price', Float),
+    Column('last_updated', TIMESTAMP)
+)
 
-with pd.read_csv(file_path, chunksize=chunk_size) as reader:
-    # print(reader)
-    for chunk in reader:
-        print(chunk)
+# Create the table in the database (generates SQL)
+metadata.create_all(engine)
+
+
+# Load the data
+file_path = './data/marketing_sample_for_walmart_com-ecommerce__20191201_20191231__30k_data.csv'
+df = pd.read_csv(file_path)
+df.fillna("Uncategorized", inplace=True)
+
+# Insert each row into the table product
+try:
+    for id, row in df.iterrows():
+    try:
+        uniq_id = str(id+1)
+        # print(f"id={id}")
+        name = row['Product Name']
+        cat = row['Category']
+        price = row['List Price']
+        datetime_str = row['Crawl Timestamp']
+        datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S %z')
+        last_updated = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
+    except KeyError as key_err:
+        logger.error(f"KeyError: {key_err}")
+        continue
+    except ValueError as value_err:
+        logger.error(f"ValueError: {value_err}")
+        continue
+    except Exception as err:
+        logger.error(f"An unexpected error occurred: {err}")
+        continue
+
+    # Insert data into the product table
+    insert_query = product_table.insert().values(
+        id=uniq_id,
+        name=name,
+        category=cat,
+        price=price,
+        last_updated=last_updated
+    )
+
+    # Execute the query
+    connection.execute(insert_query)
+    connection.commit()
+
+except Exception as err:
+    logger.error(f"An unexpected error occurred: {err}")
+finally:
+    # Close the connection
+    connection.close()
