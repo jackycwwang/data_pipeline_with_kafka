f1983bcaacf9bbe448deb1a63fb1cf0c59dde6c6
Jacky Wang <jacky@lemay.ai>
2023-10-09 12:58:44 -0400

    Updated product_updater

--- product_updater/product_updater.py
+++ product_updater/product_updater.py
@@ -1,23 +1,24 @@
 import os
 import pandas as pd
+from datetime import datetime
+import logging
+import time
+
 from sqlalchemy import create_engine, Table, Column, Integer, String, Float, TIMESTAMP, MetaData
 from sqlalchemy.sql import text
 import pymysql
-from datetime import datetime
-import logging
 
 
 logger = logging.getLogger("my_logger")
 
-# host_name =  "172.25.184.208"
 user = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWORD')
-host_name =  os.getenv('DB_HOST')
+hostname =  os.getenv('DB_HOST')
 port = os.getenv('DB_PORT')
 database = os.getenv('DB_NAME')
 
 try:
-  engine = create_engine(f"mysql+pymysql://{user}:{password}@{host_name}:{port}/{database}")
+  engine = create_engine(f"mysql+pymysql://{user}:{password}@{hostname}:{port}/{database}")
   connection = engine.connect()
 except pymysql.Error:
   logger.error(f"Database connection error: {err}")
@@ -43,49 +44,51 @@ product_table = Table('product', metadata,
 # Create the table in the database (generates SQL)
 metadata.create_all(engine)
 
-
 # Load the data
-file_path = 'data/marketing_sample_for_walmart_com-ecommerce__20191201_20191231__30k_data.csv'
+file_path = 'data/products_update.csv'
 df = pd.read_csv(file_path)
 df.fillna("Uncategorized", inplace=True)
 
-# Insert each row into the table product
+# Iterate through your data and insert it into the product table
+chunk_size = 15
 try:
-    for id, row in df.iterrows():
-        try:
-            uniq_id = str(id+1)
-            # print(f"id={id}")
-            name = row['Product Name']
-            cat = row['Category']
-            price = row['List Price']
-            datetime_str = row['Crawl Timestamp']
-            datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S %z')
+  with pd.read_csv(file_path, chunksize=chunk_size) as reader:
+    for chunk in reader:
+        for _, row in chunk.iterrows():
+          try:
+            id = row['id']
+            name = row['name']
+            cat = row['category']
+            price = row['price']
+            datetime_obj = datetime.strptime(row['last_updated'], '%Y-%m-%d %H:%M:%S')
             last_updated = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
-        except KeyError as key_err:
+          except KeyError as key_err:
             logger.error(f"KeyError: {key_err}")
             continue
-        except ValueError as value_err:
+          except ValueError as value_err:
             logger.error(f"ValueError: {value_err}")
             continue
-        except Exception as err:
+          except Exception as err:
             logger.error(f"An unexpected error occurred: {err}")
             continue
 
-        # Insert data into the product table
-        insert_query = product_table.insert().values(
-            id=uniq_id,
+          # Insert data into the product table
+          insert_query = product_table.insert().values(
+            id=id,
             name=name,
             category=cat,
             price=price,
             last_updated=last_updated
-        )
+          )
 
-        # Execute the query
-        connection.execute(insert_query)
-        connection.commit()
+          # Execute the query
+          connection.execute(insert_query)
+          connection.commit()
+          time.sleep(1)
+        time.sleep(10)
 
 except Exception as err:
-    logger.error(f"An unexpected error occurred: {err}")
+  logger.error(f"An unexpected exception occurred: {err}")
 finally:
-    # Close the connection
-    connection.close()
+  # Close the connection
+  connection.close()
